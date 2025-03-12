9e5d7112ad9a574db4959bfca681eb79b52f4278
BPChance <bradenpchance6@gmail.com>
2025-03-12 17:51:33 -0500

    GraphQL ID type and Project Type

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,7 +1,13 @@
 const { query } = require('express');
 const graphql = require('graphql');
 const _ = require('lodash');
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLID,
+  GraphQLSchema,
+} = graphql;
 
 // dummy data
 const tasks = [
@@ -21,6 +27,23 @@ const tasks = [
   },
 ];
 
+const projects = [
+  {
+    id: '1',
+    title: 'Advanced HTML',
+    weight: 1,
+    description:
+      'Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!',
+  },
+  {
+    id: '2',
+    title: 'Bootstrap',
+    weight: 1,
+    description:
+      'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.',
+  },
+];
+
 // defining the task type
 const TaskType = new GraphQLObjectType({
   name: 'Task',
@@ -32,7 +55,18 @@ const TaskType = new GraphQLObjectType({
   },
 });
 
-// defining root query
+// defining the project type
+const ProjectType = new GraphQLObjectType({
+  name: 'Project',
+  fields: {
+    id: { type: GraphQLID },
+    title: { type: GraphQLString },
+    weight: { type: GraphQLInt },
+    description: { type: GraphQLString },
+  },
+});
+
+// root query
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
@@ -43,6 +77,13 @@ const RootQuery = new GraphQLObjectType({
       },
       resolve(parent, args) {
         return _.find(tasks, { id: args.id });
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(projects, { id: args.id });
       },
     },
   },
